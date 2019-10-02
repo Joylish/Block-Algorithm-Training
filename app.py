5e6c7ca01d516b6fa5504e43ea8a3e38a1321f65
Joylish <yeonzzu45@ajou.ac.kr>
2019-10-03 02:32:38 +0900

    deploy server

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify, url_for
 import json
 from flask_sqlalchemy import SQLAlchemy
@@ -40,34 +41,34 @@ class Problem(db.Model):
         self.correctRate = correctRate
         self.initXML = initXML
 
-class Save(db.model):
-    __tablename__: "Save"
-    sid = db.Column(db.BigInteger(), primary_key= True)
-    pid = db.Column(db.BigInteger())
-    uid = db.Column(db.BigInteger())
-    savedXml = db.Column(db.text())
-    savedAt = db.Column(db.String())
-
-class Submission(db.model):
-    __tablename__: "Submission"
-    sid = db.column(db.BigInteger(), primary_key =True)
-    pid = db.column(db.BigInteger())
-    uid = db.column(db.BigInteger())
-    submittedAt = db.column(db.String())
-    sourceCode = db.column(db.String())
-
-class Testcase(db.model):
-    __tablename__: "Testcase"
-    tid = db.column(db.BigInteger(), primary_key=True)
-    pid = db.column(db.BigInteger())
-    input = db.column(db.String())
-    output = db.column(db.String())
-
-class Testresult(db.model):
-    __tablename__: "Testresult"
-    rid = db.column(db.BigInteger(), primary_key=True)
-    sid = db.column(db.BigInteger())
-    result = db.column(db.Integer())
+# class Save(db.model):
+#     __tablename__: "Save"
+#     sid = db.Column(db.BigInteger(), primary_key= True)
+#     pid = db.Column(db.BigInteger())
+#     uid = db.Column(db.BigInteger())
+#     savedXml = db.Column(db.text())
+#     savedAt = db.Column(db.String())
+#
+# class Submission(db.model):
+#     __tablename__: "Submission"
+#     sid = db.column(db.BigInteger(), primary_key =True)
+#     pid = db.column(db.BigInteger())
+#     uid = db.column(db.BigInteger())
+#     submittedAt = db.column(db.String())
+#     sourceCode = db.column(db.String())
+#
+# class Testcase(db.model):
+#     __tablename__: "Testcase"
+#     tid = db.column(db.BigInteger(), primary_key=True)
+#     pid = db.column(db.BigInteger())
+#     input = db.column(db.String())
+#     output = db.column(db.String())
+#
+# class Testresult(db.model):
+#     __tablename__: "Testresult"
+#     rid = db.column(db.BigInteger(), primary_key=True)
+#     sid = db.column(db.BigInteger())
+#     result = db.column(db.Integer())
 # 메인화면
 @app.route('/')
 def hello_world():
@@ -77,23 +78,23 @@ def hello_world():
 def view_problems(category=''):
     category = request.args.get('category')
     # query parameter category에 정수값이 지정될 때
-    # if category != '':
-    #     queryByCategory = Problem.query.filter(Problem.category == category)
-    #     problemByCategory = pd.read_sql(queryByCategory.statement, queryByCategory.session.bind)
-    #     return jsonify(data = json.loads(problemByCategory.to_json(orient='records')))
-    # else:
+    if category:
+        queryByCategory = Problem.query.filter(Problem.category == category)
+        problemByCategory = pd.read_sql(queryByCategory.statement, queryByCategory.session.bind)
+        return jsonify(data = json.loads(problemByCategory.to_json(orient='records')))
+    else:
         # 모든 problem 불러오기
-    queryInAll = Problem.query.filter(Problem.pid > 0)
-    problemInAll = pd.read_sql(queryInAll.statement, queryInAll.session.bind)
-    return jsonify(data = json.loads(problemInAll.to_json(orient='records')))
+        queryInAll = Problem.query.filter(Problem.pid > 0)
+        problemInAll = pd.read_sql(queryInAll.statement, queryInAll.session.bind)
+        return jsonify(data = json.loads(problemInAll.to_json(orient='records')))
 
 @app.route('/problems/<pid>', methods=['GET'])
 def view_each_problem(pid=''):
-
-        queryByPid = Problem.query.filter(Problem.pid == pid)
+    queryByPid = Problem.query.filter(Problem.pid == pid)
+    if queryByPid.count():
         problemByPid = pd.read_sql(queryByPid.statement,queryByPid.session.bind)
         return jsonify( data = json.loads(problemByPid.to_json(orient='records')),result = 200)
-    # else :
-    #     return jsonify(result = 404, err_msg = "Not found")
+    else :
+        return jsonify(result = 404, err_msg = "Not found")
 if __name__ == '__main__':
     app.run(debug=True)
